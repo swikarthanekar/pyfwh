fb5a0949c409d906766227bc5159636a80ddffb9
Swikar Thanekar <swikarthanekar@gmail.com>
2026-04-13 10:55:23 +0530

    Implement test for FWHSurface with invalid normals
    Add test for handling bad normals in FWHSurface

--- run_tests.py
+++ run_tests.py
@@ -42,7 +42,11 @@ check("repr", "test" in repr(s))
 
 d2 = dummy_surface(4, 10)
 d2['normals'] *= 2.0
-
+try:
+    FWHSurface.from_dict(d2)
+    check("rejects bad normals", False)
+except ValueError:
+    check("rejects bad normals", True)
 d3 = dummy_surface(4, 10)
 d3['areas'][0] = -1.0
 try:
